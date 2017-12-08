eae9be85aec4d1d9fe48cef398535d16a6da96cf
Lab User <user@wi.zut.pl>
2017-12-08 22:19:55 +0100

    ver.1

--- numbers.cpp
+++ numbers.cpp
@@ -1,75 +1,90 @@
 
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-using namespace std;
+#define MAX 20
+#define DEBUG 1
 
 int main()
 {
-    string liczba_1,liczba_2,tablica_wynik;
-    int przeniesiona_liczba,wynik_dodawania,indeks_liczba_1,indeks_liczba_2,licznik_petli,dlugosc_krotka_liczba;
+    char s1[MAX], s2[MAX], s3[MAX];
+    int p, w, i, j, k, n;
 
     // odczytujemy liczby do dodawania
+    printf("Enter two values separeted by new line:\n");
+    scanf("%s\n%s", s1, s2);
+    if(DEBUG) { printf("DEBUG s1: %s, s2: %s\n", s1, s2); }
 
-    printf("Podaj 2 liczby, które chcesz dodać, po każdej daj enter\n");
+    // obliczamy dÅugoÅci kaÅ¼dego z ÅaÅcuchÃ³w
+    i = strlen(s1);
+    j = strlen(s2);
+    if(DEBUG) { printf("DEBUG strlen(s1): %d, strlen(s2): %d\n", i, j); }
 
-    cin >> liczba_1 >> liczba_2;
-
-    // obliczamy długości każdego z łańcuchów
-
-    indeks_liczba_1 = liczba_1.length();
-    indeks_liczba_2 = liczba_2.length();
-
-    // wynik_dodawania dlugosc_krotka_liczba wyznaczamy długość najkrótszego łańcucha
-
-    dlugosc_krotka_liczba = indeks_liczba_1; if(indeks_liczba_2 < indeks_liczba_1) dlugosc_krotka_liczba = indeks_liczba_2;
-
-    // zerujemy przeniesienie oraz łańcuch wynikowy
-
-    przeniesiona_liczba = 0;
-
-    tablica_wynik = "";
-
-    // sumujemy kolejne od końca cyfry obu łańcuchów
-
-    for(licznik_petli = 1; licznik_petli <= dlugosc_krotka_liczba; licznik_petli++)
+    // w n wyznaczamy dÅugoÅÄ najkrÃ³tszego ÅaÅcucha
+    n = i;
+    if(j < i)
     {
-        wynik_dodawania  = (int)(liczba_1[--indeks_liczba_1]) + (int)(liczba_2[--indeks_liczba_2]) + przeniesiona_liczba - 96;
-        przeniesiona_liczba  = wynik_dodawania / 10;
-        tablica_wynik = (char)((wynik_dodawania % 10) + 48) + tablica_wynik;
+        n = j;
+    }
+    if(DEBUG) { printf("DEBUG n: %d\n", n); }
+
+    // zerujemy przeniesienie oraz ÅaÅcuch wynikowy
+    p = 0;
+    memset(s3, 0, MAX);
+    if(DEBUG) { printf("DEBUG memset s3: %s\n", s3); }
+
+    // sumujemy kolejne od koÅca cyfry obu ÅaÅcuchÃ³w
+    for(k = 0; k != n; k++)
+    {
+        w = s1[i-1] + s2[j-1] + p - 96;
+        if(DEBUG) { printf("DEBUG w 1st stage: %d\n", w); }
+        i = i - 1;
+        j = j - 1;
+        p = w / 10;
+        memcpy(s3+1, s3, sizeof(s3));
+        s3[0] = (char) (w % 10) + 48;
+        if(DEBUG) { printf("DEBUG s3 1st stage: %s\n", s3); }
+    }
+    if(DEBUG) { printf("DEBUG s3 1st stage complete: %s\n", s3); }
+
+    // jeÅli ÅaÅcuch s1 ma jeszcze cyfry, to dodajemy do nich
+    // przeniesienia i umieszczamy w ÅaÅcuchu wynikowym
+    while(i > 0)
+    {
+        w = s1[--i] + p - 48;
+        if(DEBUG) { printf("DEBUG w 2nd stage: %d\n", w); }
+        p = w / 10;
+        memcpy(s3+1, s3, sizeof(s3));
+        s3[0] = (char) (w % 10) + 48;
+        if(DEBUG) { printf("DEBUG s3 2nd stage: %s\n", s3); }
+    }
+    if(DEBUG) { printf("DEBUG s3 2nd stage complete: %s\n", s3); }
+
+    // jeÅli ÅaÅcuch s2 ma jeszcze cyfry, to dodajemy do nich
+    // przeniesienia i umieszczamy w ÅaÅcuchu wynikowym
+    while(j > 0)
+    {
+        w = s2[--j] + p - 48;
+        if(DEBUG) { printf("DEBUG w 3rd stage: %d\n", w); }
+        p = w / 10;
+        memcpy(s3+1, s3, sizeof(s3));
+        s3[0] = (char) (w % 10) + 48;
+        if(DEBUG) { printf("DEBUG s3 3rd stage: %s\n", s3); }
+    }
+    if(DEBUG) { printf("DEBUG s3 3rd stage complete: %s\n", s3); }
+
+    // jeÅli pozostaÅo przeniesienie, to doÅÄczamy je do cyfr
+    // w ÅaÅcuchu wynikowym
+    if(p > 0)
+    {
+        memcpy(s3+1, s3, sizeof(s3));
+        s3[0] = p + 48;
     }
 
-    // jeśli łańcuch liczba_1 ma jeszcze cyfry, to dodajemy do nich
-    // przeniesienia indeks_liczba_1 umieszczamy wynik_dodawania łańcuchu wynikowym
+    // wyÅwietlamy wynik
+    printf("s3: %s\n", s3);
 
-    while(indeks_liczba_1)
-    {
-        wynik_dodawania  = liczba_1[--indeks_liczba_1] + przeniesiona_liczba - 48;
-        przeniesiona_liczba  = wynik_dodawania / 10;
-        tablica_wynik = (char)((wynik_dodawania % 10) + 48) + tablica_wynik;
-    }
-
-    // jeśli łańcuch liczba_2 ma jeszcze cyfry, to dodajemy do nich
-    // przeniesienia indeks_liczba_1 umieszczamy wynik_dodawania łańcuchu wynikowym
-
-    while(indeks_liczba_2)
-    {
-        wynik_dodawania  = liczba_2[--indeks_liczba_2] + przeniesiona_liczba - 48;
-        przeniesiona_liczba  = wynik_dodawania / 10;
-        tablica_wynik = (char)((wynik_dodawania % 10) + 48) + tablica_wynik;
-    }
-
-    // jeśli pozostało przeniesienie, to dołączamy je do cyfr
-    // wynik_dodawania łańcuchu wynikowym
-
-    if(przeniesiona_liczba) tablica_wynik = (char)(przeniesiona_liczba + 48) + tablica_wynik;
-
-    // jeśli wynik_dodawania tablica_wynik nie ma cyfr, to wpisujemy tam 0
-
-    if(tablica_wynik == "") tablica_wynik = "0";
-
-    // wyświetlamy wynik
-
-    cout << tablica_wynik << endl;
-
-} 
+    return 0;
+}
