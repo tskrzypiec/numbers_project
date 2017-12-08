afe85897e713670f4afe25e07329eac37fc1e83c
Lab User <user@wi.zut.pl>
2017-12-08 22:37:21 +0100

    ver.2

--- numbers.cpp
+++ numbers.cpp
@@ -5,7 +5,7 @@
 #include <ctype.h>
 
 #define MAX 20
-#define DEBUG 1
+#define DEBUG 0
 
 int main()
 {
@@ -13,16 +13,16 @@ int main()
     int p, w, i, j, k, n;
 
     // odczytujemy liczby do dodawania
-    printf("Enter two values separeted by new line:\n");
+    printf("Wpisz dwie liczby poprzedzając każdą z nich enterem:\n");
     scanf("%s\n%s", s1, s2);
     if(DEBUG) { printf("DEBUG s1: %s, s2: %s\n", s1, s2); }
 
-    // obliczamy dÅugoÅci kaÅ¼dego z ÅaÅcuchÃ³w
+    // obliczamy długości każdego z łańcuchów
     i = strlen(s1);
     j = strlen(s2);
     if(DEBUG) { printf("DEBUG strlen(s1): %d, strlen(s2): %d\n", i, j); }
 
-    // w n wyznaczamy dÅugoÅÄ najkrÃ³tszego ÅaÅcucha
+    // w n wyznaczamy długość najkrótszego łańcucha
     n = i;
     if(j < i)
     {
@@ -30,12 +30,12 @@ int main()
     }
     if(DEBUG) { printf("DEBUG n: %d\n", n); }
 
-    // zerujemy przeniesienie oraz ÅaÅcuch wynikowy
+    // zerujemy przeniesienie oraz łańcuch wynikowy
     p = 0;
     memset(s3, 0, MAX);
     if(DEBUG) { printf("DEBUG memset s3: %s\n", s3); }
 
-    // sumujemy kolejne od koÅca cyfry obu ÅaÅcuchÃ³w
+    // sumujemy kolejne od końca cyfry obu łańcuchów
     for(k = 0; k != n; k++)
     {
         w = s1[i-1] + s2[j-1] + p - 96;
@@ -49,8 +49,8 @@ int main()
     }
     if(DEBUG) { printf("DEBUG s3 1st stage complete: %s\n", s3); }
 
-    // jeÅli ÅaÅcuch s1 ma jeszcze cyfry, to dodajemy do nich
-    // przeniesienia i umieszczamy w ÅaÅcuchu wynikowym
+    // jeżeli łańcuch s1 ma jeszcze cyfry, to dodajemy do nich
+    // przeniesienia i umieszczamy w łańcuchu wynikowym
     while(i > 0)
     {
         w = s1[--i] + p - 48;
@@ -62,8 +62,8 @@ int main()
     }
     if(DEBUG) { printf("DEBUG s3 2nd stage complete: %s\n", s3); }
 
-    // jeÅli ÅaÅcuch s2 ma jeszcze cyfry, to dodajemy do nich
-    // przeniesienia i umieszczamy w ÅaÅcuchu wynikowym
+    // jeżeli łańcuch s2 ma jeszcze cyfry, to dodajemy do nich
+    // przeniesienia i umieszczamy w łańcuchu wynikowym
     while(j > 0)
     {
         w = s2[--j] + p - 48;
@@ -75,16 +75,16 @@ int main()
     }
     if(DEBUG) { printf("DEBUG s3 3rd stage complete: %s\n", s3); }
 
-    // jeÅli pozostaÅo przeniesienie, to doÅÄczamy je do cyfr
-    // w ÅaÅcuchu wynikowym
+    // jeżeli pozostało przeniesienie, to dołączamy je do cyfr
+    // w łańcuchu wynikowym
     if(p > 0)
     {
         memcpy(s3+1, s3, sizeof(s3));
         s3[0] = p + 48;
     }
 
-    // wyÅwietlamy wynik
-    printf("s3: %s\n", s3);
+    // wyświetlamy wynik
+    printf("wynik: %s\n", s3);
 
     return 0;
 }
